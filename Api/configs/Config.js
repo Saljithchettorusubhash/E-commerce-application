fb6db526bb3419129f2a6bba2fb895d85e652a24
SALJITH <saljithsubash661@gmail.com>
2024-09-13 12:29:52 +0100

    completed the first phase of Api testing

--- Api/configs/Config.js
+++ Api/configs/Config.js
@@ -4,19 +4,18 @@ dotenv.config();
 export const Config = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 3000,
+
+  // Database configuration
   DB_HOST: process.env.DB_HOST || "localhost",
   DB_PASS: process.env.DB_PASS || "root",
   DB_DATABASE: process.env.DB_DATABASE || "irish_collab_db",
-  DATABASE_URL:
-    process.env.DATABASE_URL ||
-    "postgresql://root:root@localhost:5432/irish_collab_db?schema=public",
-    SECRET_JWT:process.env.SECRET_JWT || 'Saljith661',
-    
+  DATABASE_URL: process.env.DATABASE_URL || "postgresql://root:root@localhost:5432/irish_collab_db?schema=public",
 
-  MAILJET_API_KEY:
-    process.env.MAILJET_API_KEY || "d241b567c3a3128f7d073c6353cad0fc",
+  // JWT Secret
+  SECRET_JWT: process.env.SECRET_JWT || 'default_secret_jwt',
 
-  MAILJET_API_SECRET:
-    process.env.MAILJET_API_SECRET || "84891280ff1e5fdf611c1ade5e42e454",
-  MAILJET_SENDER: process.env.MAILJET_SENDER || "saljithsubhash7@gmail.com",
+  // MailJet API configuration
+  MAILJET_API_KEY: process.env.MAILJET_API_KEY || "default_mailjet_api_key",
+  MAILJET_API_SECRET: process.env.MAILJET_API_SECRET || "default_mailjet_api_secret",
+  MAILJET_SENDER: process.env.MAILJET_SENDER || "default_sender_email@example.com"
 };
